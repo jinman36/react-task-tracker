179826a5643eac8760d8172260e6cf5466106912
Jeff Inman <jeffery.inman@hotmail.com>
2021-06-24 21:01:57 -0700

    add tzsk submit

--- src/App.js
+++ src/App.js
@@ -25,9 +25,17 @@ function App() {
     },
   ])
 
+  // Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   // Delete Task
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id))
+    setTasks (tasks.filter((task) => task.id !== id))
   }
 
   // Toggle reminder
@@ -43,7 +51,7 @@ function App() {
   return (
     <div className='container'>
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'Nothing to Do'}
     </div>
   );
