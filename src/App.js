e4dd37a2d139261c270a5e6410ee216af3aa2d65
Jeff Inman <jeffery.inman@hotmail.com>
2021-06-25 18:26:49 -0700

    corrected showAddTask state to working condition

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
 
   return (
     <div className='container'>
-      <Header onAdd={90 => setShowAddTask (!showAddTask/>
+      <Header onAdd={() => setShowAddTask (!showAddTask)}/>
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'Nothing to Do'}
     </div>
